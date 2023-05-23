3ec73835f1682923652dadd975f189cd1f6b1c01
Backie <kanungojeet09@gmail.com>
2023-05-23 14:09:56 +0530

    origin updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,  
   })
